87d1f6e00b7416a10e552a28bf3eeeb685c5f9b4
Ivan <haidutski@gmail.com>
2024-11-22 23:39:58 +0300

    daily commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,15 @@
 import styles from "./Todo.module.css";
-import listIcon from "../icons/icon_list.svg";
+import { ReactComponent as DeleteIcon } from "../icons/icon_trashcan_gray.svg";
+import { ReactComponent as CheckIcon } from "../icons/icon_checkmark_gray.svg";
+import { ReactComponent as ListIcon } from "../icons/icon_list.svg";
 
 function Todo({ index, todo, deleteTodo }) {
   return (
     <div className={styles.todoItem} onDoubleClick={(e) => deleteTodo(index)}>
-      <img src={listIcon} />
-      {todo}
+      <ListIcon className={styles.listIcon} />
+      <div className={styles.todoText}>{todo}</div>
+      <DeleteIcon className={styles.deleteIcon} />
+      <CheckIcon className={styles.checkIcon} />
     </div>
   );
 }
